a5d70daac50e1f13d0084a329c5936be26b6216f
fprager <wesayso+github@pm.me>
2020-11-20 22:46:06 +0100

    added super crazy randomizers

--- pages/admin.js
+++ pages/admin.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import ReactMarkdown from 'react-markdown';
 
 import {
-  Select, Button, Divider, InputNumber, Row, Col, Skeleton, Input,
+  Select, Button, Divider, InputNumber, Row, Col, Skeleton, Input, Space,
 } from 'antd';
 
 import Data from '../mock/data.json';
@@ -11,6 +11,50 @@ const { TextArea } = Input;
 
 const { Option } = Select;
 
+const shuffle = (array) => {
+  let currentIndex = array.length; let temporaryValue; let
+    randomIndex;
+
+  // While there remain elements to shuffle...
+  while (currentIndex !== 0) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+};
+
+const formatWords = (words, symbol) => words.split(', ').reduce((str, w, index) => (
+  `${str}
+  ${index + 1}. ${symbol}${w}${symbol}`
+), '');
+
+const randomMapTeams = () => {
+  const fabi = Data.players.find((p) => p.id === 'fabi');
+  const dani = Data.players.find((p) => p.id === 'dani');
+  const restPlayers = Data.players.filter((p) => p.id !== 'dani' && p.id !== 'fabi');
+  shuffle(restPlayers);
+  const half = Math.ceil(restPlayers.length / 2);
+  const firstHalf = restPlayers.splice(0, half);
+  const secondHalf = restPlayers.splice(-half);
+  return [[dani, ...firstHalf], [fabi, ...secondHalf]];
+};
+
+const randomTeams = () => {
+  const players = [...Data.players];
+  shuffle(players);
+  const half = Math.ceil(players.length / 2);
+  const firstHalf = players.splice(0, half);
+  const secondHalf = players.splice(-half);
+  return [firstHalf, secondHalf];
+};
+
 export default () => {
   const [scores, setScores] = useState({});
   const [rules, setRules] = useState('');
@@ -18,6 +62,29 @@ export default () => {
   const [gameId, setGameId] = useState(Data.games[0].id);
   const [loading, setLoading] = useState(false);
   const [savings, setSavings] = useState({});
+
+  // general team stuff
+  const [outputTeams, setOutputTeams] = useState('');
+
+  const formatRandomTeams = () => {
+    const [teamO, teamB] = randomTeams();
+    setOutputTeams(`${teamO.map((p) => p.name).join(', ')}#${teamB.map((p) => p.name).join(', ')}`);
+  };
+
+  // maps stuff
+  const [outputMapTeams, setOutputMapTeams] = useState('');
+  const [inputMapWords, setInputMapWords] = useState('?, ?, ?, ?, ?, ?, ?, ?, ?, ?#?, ?, ?, ?, ?, ?, ?, ?, ?, ?');
+  const [outputMapWords, setOutputMapWords] = useState('');
+
+  const formatInputMapWords = () => {
+    const [orangeWords, blueWords] = inputMapWords.split('#');
+    setOutputMapWords(`${formatWords(orangeWords, '*')}#${formatWords(blueWords, '**')}`);
+  };
+
+  const formatRandomMapTeams = () => {
+    const [danisTeam, fabisTeam] = randomMapTeams();
+    setOutputMapTeams(`${fabisTeam.map((p) => p.name).join(', ')}#${danisTeam.map((p) => p.name).join(', ')}`);
+  };
 
   const requestGameData = async () => {
     setLoading(true);
@@ -93,6 +160,60 @@ export default () => {
     return () => {};
   }, [gameId]);
 
+  const MapsTeams = (
+    <>
+      <Divider />
+      <Row gutter={[10, 10]}>
+        <Col span="11">
+          <Input addonBefore="Team Orange (Fabi): " value={outputMapTeams.split('#')[0]} readOnly />
+        </Col>
+        <Col span="2" />
+        <Col span="11">
+          <Input addonBefore="Team Blue (Dani): " value={outputMapTeams.split('#')[1]} readOnly />
+        </Col>
+      </Row>
+      <Row justify="center">
+        <Col><Button onClick={formatRandomMapTeams}>Shuffle Teams</Button></Col>
+      </Row>
+
+      <Divider />
+      <Row gutter={[10, 10]}>
+        <Col span="11">
+          <Input addonBefore="Danis Words: " value={inputMapWords.split('#')[0]} onChange={(e) => (setInputMapWords(`${e.target.value}#${inputMapWords.split('#')[1]}`))} />
+          <TextArea addonBefore="Formatted Words: " value={outputMapWords.split('#')[0]} readOnly />
+        </Col>
+        <Col span="2" />
+        <Col span="11">
+          <Input addonBefore="Fabis Words: " value={inputMapWords.split('#')[1]} onChange={(e) => (setInputMapWords(`${inputMapWords.split('#')[0]}#${e.target.value}`))} />
+          <TextArea addonBefore="Formatted Words: " value={outputMapWords.split('#')[1]} readOnly />
+        </Col>
+      </Row>
+      <Row justify="center">
+        <Col>
+          <Button onClick={formatInputMapWords}>Format Words as Numbered List</Button>
+        </Col>
+      </Row>
+    </>
+  );
+
+  const SplitTeams = (
+    <>
+      <Divider />
+      <Row gutter={[10, 10]}>
+        <Col span="11">
+          <Input addonBefore="Orange Team: " value={outputTeams.split('#')[0]} readOnly />
+        </Col>
+        <Col span="2" />
+        <Col span="11">
+          <Input addonBefore="Blue Team: " value={outputTeams.split('#')[1]} readOnly />
+        </Col>
+      </Row>
+      <Row justify="center">
+        <Col><Button onClick={formatRandomTeams}>Shuffle Teams</Button></Col>
+      </Row>
+    </>
+  );
+
   const GameSettings = (
     <>
       <Divider><div className="headline">Rules</div></Divider>
@@ -121,6 +242,12 @@ export default () => {
         </Col>
       </Row>
       <Button loading={savings.notes} onClick={saveNotes}>Save</Button>
+      {
+            gameId === 'maps' && MapsTeams
+          }
+      {
+            (gameId === 'ball' || gameId === 'cs') && SplitTeams
+          }
       <Divider><div className="headline">Scores</div></Divider>
       {
         Object.keys(scores).map((pId) => {
